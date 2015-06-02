c41cac62e28f36997dfd16f11ff3820a0ab0feb3
zz <zz@zzdeMac.local>
2015-06-02 21:25:03 +0800

    Initial Commit

---
+++ XiuQiuJi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiuQiuJi
+//
+//  Created by zz on 15-6-2.
+//  Copyright (c) 2015年 zz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XiuQiuJi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiuQiuJi
+//
+//  Created by zz on 15-6-2.
+//  Copyright (c) 2015年 zz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
